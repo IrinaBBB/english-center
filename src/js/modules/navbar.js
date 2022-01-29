ed92452cd7ba54fb9c2790a19eb1d90cb9b51a97
Irina <balkarovairina@gmail.com>
2022-01-29 20:33:58 +0100

    add: navbar (flexbox)

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -1,10 +1,14 @@
 const navbar = () => {
-    const menu = document.querySelector('.menu-toggler');
+    const menu = document.querySelector('.toggler');
     const navbar = document.querySelector('.navigation');
+    const navListItems = document.querySelectorAll('.navigation__item');
 
     menu.addEventListener('click', () => {
        navbar.classList.toggle('toggle');
        menu.classList.toggle('toggle');
+       navListItems.forEach(item => {
+           item.classList.toggle('toggle');
+       });
     });
 };
 
